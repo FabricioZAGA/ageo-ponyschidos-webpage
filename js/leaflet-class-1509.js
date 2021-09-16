ccd54ea193503443facdd12fb8b5f72211987e2b
Fabricio ZAGA <devzaga@gmail.com>
2021-09-15 21:07:22 -0500

    add: geo json on-class practice

---
+++ js/leaflet-class-1509.js
@@ -0,0 +1,28 @@
+const leafletMap = L.map('divMap').setView(
+  [21.182084609059913, -101.71251643449067],
+  17
+);
+
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+  maxZoom: 19,
+  attribution:
+    '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+}).addTo(leafletMap);
+
+const _onEachFeature = (feature, layer) => {
+  var popupContent = '';
+  feature.properties.popupContent
+    ? (popupContent = feature.properties.popupContent)
+    : (popupContent = `I'am a ${feature.geometry.type}`);
+
+  if (feature.properties.style) layer.setStyle(feature.properties.style);
+
+  layer.bindPopup(popupContent);
+};
+
+L.geoJSON(granJardin, {
+  filter: (feature, layer) => {
+    if (feature.properties) return true;
+  },
+  onEachFeature: _onEachFeature,
+}).addTo(leafletMap);
